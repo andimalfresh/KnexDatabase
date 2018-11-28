661ab1ced77f6531a0771ec92d3e0f68555f94f3
unknown <w.andrew.pedersen@gmail.com>
2018-11-28 15:13:49 -0700

    :id Route added son!!

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3535;
+const port = process.env.PORT || 5000;
 const queries = require("./queries");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(cors);
-app.use(bodyParser);
+app.use(cors());
+app.use(bodyParser.json());
 
 app.listen(port, console.log(`Broadcasting on ${port}`));
+
+app.get("/:id", (req, res) => {
+  queries.getById(req.params.id).then(students => res.send(students));
+});
 
 app.get("/", (req, res) => {
   queries.listAll().then(students => res.send(students));
--- queries.js
+++ queries.js
@@ -3,5 +3,8 @@ const database = require("./database-connection");
 module.exports = {
   listAll() {
     return database("old_students");
+  },
+  getById(id) {
+    return database("old_students").where("id", id);
   }
 };
