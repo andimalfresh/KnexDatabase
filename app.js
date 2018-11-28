8580d0cc03124bc2fb8ebcf7dfa3436c5cb83344
unknown <w.andrew.pedersen@gmail.com>
2018-11-28 13:02:40 -0700

    Populated from Database from Express routing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3535;
+const queries = require("./queries");
 
 app.listen(port, console.log(`Broadcasting on ${port}`));
 
 app.get("/", (req, res) => {
-  res.send("We Ballin Now Dog !!");
+  queries.listAll().then(students => res.send(students));
 });
