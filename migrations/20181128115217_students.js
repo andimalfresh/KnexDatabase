7500217bcc26e6a73dc0ba83ccc8faa4d1aa7314
unknown <w.andrew.pedersen@gmail.com>
2018-11-28 12:28:23 -0700

    Knex working now and schema

---
+++ migrations/20181128115217_students.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("old_students", student => {
+    student.increments("id"),
+      student.string("name"),
+      student.string("fave_animal"),
+      student.string("previous_occupation"),
+      student.float("hometown_lat", 8, 8),
+      student.float("hometown_long", 8, 8),
+      student.string("useless_superpower");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("old_students");
+};
